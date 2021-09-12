cfdf6e00efbae5f2039df1f17ea7d93506643ad1
Hugo Rodger-Brown <hugo@yunojuno.com>
2021-09-12 17:46:03 +0100

    Update AppConfig with correct name

--- frozen_field/apps.py
+++ frozen_field/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class FrozenDataConfig(AppConfig):
-    name = "frozen"
+    name = "frozen_field"
--- tests/settings.py
+++ tests/settings.py
@@ -21,7 +21,7 @@ INSTALLED_APPS = (
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "frozen",
+    "frozen_field",
     "tests",
 )
 
